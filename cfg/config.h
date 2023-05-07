cc5c6af748863faca6d379d5099a3991f6a6bb7d
Nicolas Schurando <nicolas@sitronlabs.com>
2023-05-08 00:36:05 +0200

    Updated pins and configuration for r2.

--- cfg/config.h
+++ cfg/config.h
@@ -1,17 +1,27 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-/* MySensors configuration */
+/* Leds configuration */
+#define CONFIG_LED_GREEN_PIN 6
+#define CONFIG_LED_YELLOW_PIN 5
+#define CONFIG_LED_RED_PIN 4
+
+/* MySensors configuration
+ * @see https://www.mysensors.org/build/advanced_gateway */
 #define MY_DEBUG
-#define MY_RADIO_RF24  // Enable and select radio type attached
+#define MY_RADIO_RF24
+#define MY_RF24_PA_LEVEL RF24_PA_MAX
 #define MY_RF24_CE_PIN 9
 #define MY_RF24_CS_PIN 10
-#define MY_GATEWAY_SERIAL                // Enable serial gateway
-#define MY_INCLUSION_MODE_FEATURE        // Enable inclusion mode
-#define MY_INCLUSION_MODE_DURATION 60    // Set inclusion mode duration (in seconds)
-#define MY_DEFAULT_LED_BLINK_PERIOD 300  // Set blinking period
-#define MY_DEFAULT_ERR_LED_PIN 4         // Error led pin
-#define MY_DEFAULT_RX_LED_PIN 6          // Receive led pin
-#define MY_DEFAULT_TX_LED_PIN 5          // the PCB, on board LED
+#define MY_GATEWAY_FEATURE
+#define MY_GATEWAY_SERIAL
+#define MY_INCLUSION_MODE_FEATURE
+#define MY_INCLUSION_BUTTON_FEATURE
+#define MY_INCLUSION_MODE_DURATION 60
+#define MY_INCLUSION_MODE_BUTTON_PIN 7
+#define MY_INCLUSION_LED_PIN CONFIG_LED_YELLOW_PIN
+#define MY_DEFAULT_LED_BLINK_PERIOD 300
+#define MY_DEFAULT_ERR_LED_PIN CONFIG_LED_RED_PIN
+#define MY_DEFAULT_RX_LED_PIN CONFIG_LED_GREEN_PIN
 
 #endif
